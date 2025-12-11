c01bcb0b746f22846fe773b4eb2843efca818993
Pedro Henrique de Borba <Henrique@MacBook-Air-de-Pedro.local>
2025-12-11 00:27:52 -0300

    Adicionado Handler Exception -- Pedro Borba

--- src/main/java/br/com/HEALTHTRACK/API/HEALTHTRACK/Repository/PacienteRepository.java
+++ src/main/java/br/com/HEALTHTRACK/API/HEALTHTRACK/Repository/PacienteRepository.java
@@ -6,4 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PacienteRepository extends JpaRepository<Paciente,Long> {
+
+    Paciente findByCpf(String cpf);
+    Paciente findByEmail(String email);
 }
--- src/main/java/br/com/HEALTHTRACK/API/HEALTHTRACK/Service/PacienteService.java
+++ src/main/java/br/com/HEALTHTRACK/API/HEALTHTRACK/Service/PacienteService.java
@@ -1,16 +1,42 @@
 package br.com.HEALTHTRACK.API.HEALTHTRACK.Service;
 
+import br.com.HEALTHTRACK.API.HEALTHTRACK.DTO.Paciente.PacienteCadastroDTO;
+import br.com.HEALTHTRACK.API.HEALTHTRACK.Entity.Paciente;
+import br.com.HEALTHTRACK.API.HEALTHTRACK.Mapper.PacienteMapper;
 import br.com.HEALTHTRACK.API.HEALTHTRACK.Repository.PacienteRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Service
 public class PacienteService {
 
     @Autowired
     private PacienteRepository pacienteRepositoryOficial;
+    @Autowired
+    PacienteMapper pacienteMapper;
 
-    public PacienteRepository getPacienteRepositoryOficial() {
-        return pacienteRepositoryOficial;
+    public void cadastrarPaciente(@Valid PacienteCadastroDTO pacienteDTO){
+        try{
+            if(pacienteRepositoryOficial.findByCpf(pacienteDTO.cpf()) != null){
+                // Erro CPF Throw new Excpetion
+            }
+
+            if(pacienteRepositoryOficial.findByEmail(pacienteDTO.email()) != null){
+                // Erro EMAIL Throw new Exception
+            }
+
+            if(pacienteDTO.dataNascimento().isBefore(LocalDate.now())){
+                // Erro Data Nascimento Futura Throw new Excpetion
+            }
+
+            Paciente pacienteConvertido = pacienteMapper.convertePacienteEntidadeCadastro(pacienteDTO);
+            pacienteRepositoryOficial.save(pacienteConvertido);
+        }catch(Exception e){
+
+        }
     }
 }
