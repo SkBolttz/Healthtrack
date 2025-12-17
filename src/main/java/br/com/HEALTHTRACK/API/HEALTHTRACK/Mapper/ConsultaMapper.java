1f840c05aa0487cefc4625dde9d644fea1086c75
pedrooDevV <pedromplctitto@gmail.com>
2025-12-17 00:17:59 -0300

    [Pedro Costa] Começando Service de Consulta

---
+++ src/main/java/br/com/HEALTHTRACK/API/HEALTHTRACK/Mapper/ConsultaMapper.java
@@ -0,0 +1,10 @@
+package br.com.HEALTHTRACK.API.HEALTHTRACK.Mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+public interface ConsultaMapper {
+    
+
+}
